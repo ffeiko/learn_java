1394b33a26b0638a562e4e64da367c9489e2b4f3
ffeiko <wangjingffei@126.com>
2017-11-15 18:02:47 +0800

    提交

---
+++ thinkInJava02/src/main/java/com/ffeiko/decode/CountDownLatch/test03/Worker.java
@@ -0,0 +1,43 @@
+package com.ffeiko.decode.CountDownLatch.test03;
+
+import java.util.concurrent.CountDownLatch;
+
+/**
+ * Created by Administrator on 2017/11/15.
+ */
+public class Worker  extends  Thread{
+
+    //工作者名
+    private String name;
+    //第一阶段工作时间
+    private long time;
+
+    private CountDownLatch countDownLatch;
+
+    public Worker(String name, long time, CountDownLatch countDownLatch) {
+        this.name = name;
+        this.time = time;
+        this.countDownLatch = countDownLatch;
+    }
+
+    @Override
+    public void run() {
+        // TODO 自动生成的方法存根
+        try {
+            System.out.println(name+"开始工作");
+            Thread.sleep(time);
+            System.out.println(name+"第一阶段工作完成");
+
+            countDownLatch.countDown();
+
+            Thread.sleep(2000); //这里就姑且假设第二阶段工作都是要2秒完成
+            System.out.println(name+"第二阶段工作完成");
+            System.out.println(name+"工作完成，耗费时间="+(time+2000));
+
+        } catch (InterruptedException e) {
+            // TODO 自动生成的 catch 块
+            e.printStackTrace();
+        }
+    }
+
+}
