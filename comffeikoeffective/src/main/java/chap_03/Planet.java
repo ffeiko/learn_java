58adea5767689b0fd137d2a56dbba533a7c2f68e
ffeiko <wangjingffei@126.com>
2018-03-13 16:04:16 +0800

    提交

---
+++ comffeikoeffective/src/main/java/chap_03/Planet.java
@@ -0,0 +1,31 @@
+package chap_03;
+
+/**
+ * @author wangjingffei
+ * @DESCRIPTION
+ * @create 2018/3/13
+ */
+public class Planet {
+//    MERCURY(3.302e+23,2.439e6),
+//    VENUS(3.302e+23,2.439e6),
+//    EARTH(3.302e+23,2.439e6),
+//    mars(3.302e+23,2.439e6),
+//    jupiter(3.302e+23,2.439e6),
+//    saturn(3.302e+23,2.439e6),
+//    uranus(3.302e+23,2.439e6),
+//    neptune(3.302e+23,2.439e6);
+//    private final double mass;
+//    private final double radius;
+//    private final double surfaceGravity;
+//
+//    private static final double G = 6.67300E -11;
+//
+//    public Planet(double mass, double radius, double surfaceGravity) {
+//        this.mass = mass;
+//        this.radius = radius;
+//        this.surfaceGravity = surfaceGravity;
+//    }
+
+
+
+}
---
+++ comffeikoeffective/src/main/java/chap_06/Phase.java
@@ -0,0 +1,20 @@
+package chap_06;
+
+/**
+ * @author wangjingffei
+ * @DESCRIPTION
+ * @create 2018/3/13
+ */
+public class Phase {
+//    public enum TRANSITION {
+//        MELT,FREEZE,BOIL,CONDENSE,SUBLIME,DEPOSTIT;
+//        //
+//        private static final transition[][] transitions = {
+//                {null, MELT,SUBLIME},{FREEZE ,null , BOIL},{DEPOSTIT ,CONDENSE,null}
+//        };
+//    }
+//    public static  transition from (Phase src , phase dst){
+//        return transitions[src. ordinal()][dst.ordinal()];
+//    }
+
+}
